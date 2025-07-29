fe9184539e876c22ed044ff42b1a66d713eafe33
HariKrishna Annangi <harikrishna.annangi@grainger.com>
2025-07-29 14:12:28 -0400

    changed the author

--- src/main/java/io/javabrains/reactiveworkshop/User.java
+++ src/main/java/io/javabrains/reactiveworkshop/User.java
@@ -1,50 +1,50 @@
 package io.javabrains.reactiveworkshop;
 
 /**
- * This class is a sample custom Java type used in the exercises.
- * DO NOT MODIFY THIS CODE
+ * This class is a sample custom Java type used in the exercises. DO NOT MODIFY THIS CODE
  *
- * @author koushikkothagal
+ * @author hannangi
  */
 
 public class User {
 
-    private int id;
-    private String firstName;
-    private String lastName;
+  private int id;
+  private String firstName;
+  private String lastName;
 
-    public User(int id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  public User(int id, String firstName, String lastName) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getFirstName() {
-        return firstName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  public String getLastName() {
+    return lastName;
+  }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-    @Override
-    public String toString() {
-        return "User{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + '}';
-    }
+  @Override
+  public String toString() {
+    return "User{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName
+        + '\'' + '}';
+  }
 }
